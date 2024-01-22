d078bf9aad58f703e2d634d9c0b1d2e2543e397a
shinminkyoung1 <dottiee0702@gmail.com>
2024-01-22 22:11:18 +0900

    7장 게시글 수정하기: Update

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -19,6 +19,7 @@ import java.util.ArrayList;
 public class ArticleController {
     @Autowired // 스프링 부트가 미리 생성해 놓은 리파지터리 객체 주입(DI)
     private ArticleRepository articleRepository;
+
     @GetMapping("/articles/new")
     public String newArticleForm() {
         return "articles/new";
@@ -49,6 +50,7 @@ public class ArticleController {
         // 3. 뷰 페이지 반환하기
         return "articles/show";  // 목록으로 돌아가기 링크를 넣을 뷰 파일 확인
     }
+
     @GetMapping("/articles")
     public String index(Model model) {
         // 1. 모든 데이터 가져오기
@@ -58,4 +60,32 @@ public class ArticleController {
         // 3. 뷰 페이지 설정하기
         return "articles/index";
     }
+
+    @GetMapping("/articles/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+        // 수정할 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        // 모델에 데이터 등록하기
+        model.addAttribute("article",articleEntity);
+        // 뷰 페이지 설정하기
+        return "articles/edit";
+    }
+
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form) {  // 매개변수로 DTO 받아 오기
+        log.info(form.toString());
+        // 1. DTO를 엔티티로 변환하기
+        Article articleEntity = form.toEntity(); // DTO(form)를 엔티티(articleEntity)로 변환하기
+        log.info(articleEntity.toString());  // 엔티티로 잘 변환됐는지 로그 찍기
+        // 2. 엔티티를 DB에 저장하기
+        // 2-1. DB에서 기존 데이터 가져오기
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+        // 2-2. 기존 데이터 값을 갱신하기
+        if (target != null) {
+            articleRepository.save(articleEntity);  // 엔티티를 DB에 저장 (갱신)
+        }
+        // 3. 수정 결과 페이지로 리다이렉트하기
+        return "redirect:/articles/" + articleEntity.getId();
+    }
 }
+
